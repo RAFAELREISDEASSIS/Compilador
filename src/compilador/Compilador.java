42cc013cd9357d55370d5d7dbf990b988963f0a4
MatheusCostaRezende <math3uscosta@gmail.com>
2014-05-17 13:36:05 -0300

    Estamos começando :)

--- src/compilador/Compilador.java
+++ src/compilador/Compilador.java
@@ -1,25 +1,54 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package compilador;
 
-import java.time.Clock;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PushbackReader;
+import java.io.Reader;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import meuprojetoLFT.lexer.Lexer;
+import meuprojetoLFT.lexer.LexerException;
+import meuprojetoLFT.node.EOF;
+import meuprojetoLFT.node.TEspacoembranco;
+import meuprojetoLFT.node.Token;
 
-/**
- *
- * @author Rafael Reis
- */
 public class Compilador {
 
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws LexerException, IOException {
         // TODO code application logic here
-        System.out.println("Chega de testes, agora vai....huuummm");
+        Reader r;
+        try {
+            for (int i = 0; i < args.length; i++) {
+                System.out.println(" O Arquivo testado é: '" + args[i] + "'\n");
+                r = new FileReader(args[i]);
+                Lexer l = new Lexer(new PushbackReader(r));
+                while (true) {
+                    Token t = l.next();
+                    String name = t.getClass().getSimpleName();
+                    if (!name.equals("EOF")) {
+                        name=name.substring(1, name.length());
+                    }
+                    if (!(t instanceof TEspacoembranco)){
+                        if (!name.equals("EOF")){
+                        System.out.print(name);}
+//                        System.out.println("aqui");
+                    }else{
+//                        System.out.println("aqui2");
+                        System.out.print(t.getText());
+                    }
+                    if(t instanceof EOF)
+                        break;     
+                }
+                System.out.println("\n|o||o||o|o|o|\n");
+            }
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(Compilador.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
     }
-    
+
 }
